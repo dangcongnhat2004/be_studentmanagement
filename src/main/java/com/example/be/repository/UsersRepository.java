c36fbfcee26e79f4c678d24cbf0b48d5afe5f46a
DANG CONG NHAT <dangcongnhat2004qb@gmail.com>
2024-06-12 00:14:41 +0700

    update security

--- src/main/java/com/example/be/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.be.repository;
-
-import com.example.be.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users,Integer> {
-}
